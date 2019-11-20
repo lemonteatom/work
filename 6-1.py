82f5b9683931aec0ad0a34f70031bb71825c545d
lemonteatom <lemontea102424@gmail.com>
2019-11-20 11:45:27 +0900

    hoge

---
+++ 6-1.py
@@ -0,0 +1,8 @@
+def showData(year,month,day):
+    print(year,"年",month,"月",day,"日")
+
+year = input("誕生年を入力してください。")
+month = input("誕生月を入力してください。")
+day = input("誕生日を入力してください。")
+
+showData(year,month,day)
